248aef5cfa91a14ad69cb9409d6b897598e334d4
atharvadeshmukh <atharva.deshmukh@neebal.com>
2023-10-28 19:49:09 +0530

    in js changes

--- microproject/todomicro.js
+++ microproject/todomicro.js
@@ -1,18 +1,26 @@
-const inputText = document.getElementById("inputtext");
+const inputBox = document.getElementById("inputtext");
 
-const list = document.getElementById("list1");
+const listContainer = document.getElementById("list1");
 
 function fun() {
-  if (inputText.value === "") {
+  if (inputBox.value === "") {
     alert("Pls enter your task");
   } else {
     let li = document.createElement("li");
-    li.innerHTML = inputText.value;
+    li.innerHTML = inputBox.value;
 
-    list.appendChild(li);
+    listContainer.appendChild(li);
     let span = document.createElement("span");
     span.innerHTML = "\u00d7";
     li.appendChild(span);
   }
-  inputText.value = ""; //for clearing input text
+  inputBox.value = ""; //for clearing input text
 }
+
+// listContainer.addEventListener("click",function(e)){
+
+//   if(e.target.tagname==="li")
+//   {
+//       e.target.classList.toggle="checked";
+//   }
+// };
