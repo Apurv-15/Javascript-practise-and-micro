d6789c002d912eefba476fd2cc686a33f81418f1
atharvadeshmukh <atharva.deshmukh@neebal.com>
2023-11-01 16:04:42 +0530

    final code

--- microproject/todomicro.js
+++ microproject/todomicro.js
@@ -1,13 +1,15 @@
 const inputBox = document.getElementById("inputtext");
 
 const listContainer = document.getElementById("list1");
+const prioritySelect = document.getElementById("priority");
 
 function fun() {
   if (inputBox.value === "") {
     alert("Pls enter your task");
   } else {
+    const priority = prioritySelect.value;
     let li = document.createElement("li");
-    li.innerHTML = inputBox.value;
+    li.innerHTML = `${inputBox.value} (Priority: ${priority})`;
 
     listContainer.appendChild(li);
     let span = document.createElement("span");
@@ -17,10 +19,25 @@ function fun() {
   inputBox.value = ""; //for clearing input text
 }
 
-// listContainer.addEventListener("click",function(e)){
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      cook();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      cook();
+    }
+  },
+  false
+);
 
-//   if(e.target.tagname==="li")
-//   {
-//       e.target.classList.toggle="checked";
-//   }
-// };
+function cook() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function fetch() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+fetch();
