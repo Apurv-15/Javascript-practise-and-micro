070f63b32ba469f83170e801d7a93d615da37106
atharvadeshmukh <atharva.deshmukh@neebal.com>
2023-11-02 09:36:49 +0530

    priority legend assigned

--- microproject/todomicro.js
+++ microproject/todomicro.js
@@ -1,32 +1,58 @@
 const inputBox = document.getElementById("inputtext");
-
-const listContainer = document.getElementById("list1");
 const prioritySelect = document.getElementById("priority");
 
-function fun() {
-  if (inputBox.value === "") {
-    alert("Pls enter your task");
-  } else {
-    const priority = prioritySelect.value;
-    let li = document.createElement("li");
-    li.innerHTML = `${inputBox.value} (Priority: ${priority})`;
+function addDeleteButton(li) {
+  // Create a delete button for the task
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "\u00d7";
+  deleteButton.className = "delete-button"; // Add a class for styling
+  li.appendChild(deleteButton);
 
-    listContainer.appendChild(li);
-    let span = document.createElement("span");
-    span.innerHTML = "\u00d7";
-    li.appendChild(span);
-  }
-  inputBox.value = ""; //for clearing input text
+  deleteButton.addEventListener("click", function () {
+    li.remove();
+    cook();
+  });
 }
 
-listContainer.addEventListener(
+function fun() {
+  const inputText = inputBox.value;
+  const priority = prioritySelect.value;
+  if (inputText.trim() === "") {
+    alert("Please enter your task");
+  } else {
+    // Create a new list item for the task
+    const li = document.createElement("li");
+    li.textContent = `${inputText} (Priority: ${priority})`;
+
+    // Add a delete button to the list item
+    addDeleteButton(li);
+
+    // Append the task to the corresponding priority fieldset
+    let taskContainer;
+    if (priority === "high") {
+      taskContainer = document.getElementById("high-priority-tasks");
+    } else if (priority === "medium") {
+      taskContainer = document.getElementById("medium-priority-tasks");
+    } else if (priority === "low") {
+      taskContainer = document.getElementById("low-priority-tasks");
+    }
+
+    taskContainer.appendChild(li);
+
+    // Clear the input text
+    inputBox.value = "";
+
+    // Save the updated task list to local storage
+    cook();
+  }
+}
+
+// Event listener for marking tasks as checked
+document.getElementById("list1").addEventListener(
   "click",
   function (e) {
     if (e.target.tagName === "LI") {
       e.target.classList.toggle("checked");
-      cook();
-    } else if (e.target.tagName === "SPAN") {
-      e.target.parentElement.remove();
       cook();
     }
   },
@@ -34,10 +60,25 @@ listContainer.addEventListener(
 );
 
 function cook() {
-  localStorage.setItem("data", listContainer.innerHTML);
+  const highPriorityTasks = document.getElementById("high-priority-tasks").innerHTML;
+  const mediumPriorityTasks = document.getElementById("medium-priority-tasks").innerHTML;
+  const lowPriorityTasks = document.getElementById("low-priority-tasks").innerHTML;
+
+  const taskData = {
+    high: highPriorityTasks,
+    medium: mediumPriorityTasks,
+    low: lowPriorityTasks,
+  };
+
+  localStorage.setItem("data", JSON.stringify(taskData));
 }
 
 function fetch() {
-  listContainer.innerHTML = localStorage.getItem("data");
+  const taskData = JSON.parse(localStorage.getItem("data")) || {};
+  document.getElementById("high-priority-tasks").innerHTML = taskData.high || "";
+  document.getElementById("medium-priority-tasks").innerHTML = taskData.medium || "";
+  document.getElementById("low-priority-tasks").innerHTML = taskData.low || "";
 }
+
+// Call the fetch function to display tasks from local storage when the page loads
 fetch();
